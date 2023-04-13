80bd2ee5fbe71cda3bce82a976f7074b0f40d920
Ilya Chernyaev <ilyachernyaev750@gmail.com>
2023-04-13 10:01:26 +0300

    first commit

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,30 +1,31 @@
 import numpy as np
-from numpy.typing import ArrayLike
-from numpy import cos, sin
 import pandas as pd
 from src.data.make_dataset import DataSet
 
 
-
-def calc_rot_angle(x0: float, y0: float, x1: float, y1: float) -> tuple[float, float]:
+def calc_rot_angle(x0: float,
+                   y0: float,
+                   x1: float,
+                   y1: float) -> tuple[float, float]:
     """
     Calculates rotation angle to switch to vehicle local reference frame
-    
+
     Parameters
     ----------
     x0, y0: float
         Coordinates of the point at time t-1
-        
+
     x1, y1: float
         Coordinates of the point at time t
-        
+
     Returns
     ----------
     alpha_x: float
-        Angle of rotation to switch from global to local reference frame (from x_glob to x_loc)
-        
+        Angle of rotation to switch from global to local reference frame
+
     alpha_ y: float
     """
+
     y_delta = y1-y0
     x_delta = x1-x0
     alpha_x = np.arctan2(y_delta, x_delta)
@@ -32,316 +33,364 @@ def calc_rot_angle(x0: float, y0: float, x1: float, y1: float) -> tuple[float, f
     return alpha_x, alpha_y
 
 
-def caluclate_shifts(roll_deg: float, pitch_deg:float, height: float = 1500) -> tuple[float, float]:
+def caluclate_shifts(roll_deg: float,
+                     pitch_deg: float,
+                     height: float = 1500) -> tuple[float, float]:
     """
-    Calculates values and directions of shifts along the axes of vehicle local reference frame for tilt compensation.
-    
+    Calculates values and directions of shifts along the axes of vehicle
+    local reference frame for tilt compensation.
+
     Parameters
     ----------
     roll_deg: float
         Tilt along y-axis of the vehicle local reference frame in degrees.
-        Positive roll corresponds to the orientation when the right side of the vehicle is being lower than the left side.
-        
+        Positive roll corresponds to the orientation when the right side of
+        the vehicle is being lower than the left side.
+
     pitch_deg: float
         Tilt along x-axis of the vehicle local reference frame in degrees.
-        Positive pitch corresponds to the orientation when the front part of the vehicle is being lower than the rear part.
-        
+        Positive pitch corresponds to the orientation when the front part of
+        the vehicle is being lower than the rear part.
+
     height: float
         Height of GNSS module installation in mm.
-        
+
     Returns
     ----------
     x_shift: float
         shift along x-axis of local frame in mm.
-        
+
     y_shift: float
         shift along y-axis of local frame in mm.
     """
-    
+
     y_shift = np.abs(height * np.sin(np.deg2rad(roll_deg)))
     height_adjusted = np.abs(height * np.cos(np.deg2rad(roll_deg)))
     x_shift = np.abs(height_adjusted * np.sin(np.deg2rad(pitch_deg)))
     return x_shift, y_shift
 
+
 def calculate_backroll(roll_deg: float, height: float = 1500) -> float:
     """
     Calculates shift along y-axis in the backward direction
     """
+
     y_shift = -np.abs(height * np.sin(np.deg2rad(roll_deg)))
+
     return y_shift
 
 
-def apply_shifts(x: float, y: float,
-                 alpha_x: float, alpha_y: float,
-                 shift_x: float = 0, shift_y: float = 0) -> tuple[float, float]:
+def apply_shifts(x: float,
+                 y: float,
+                 alpha_x: float,
+                 alpha_y: float,
+                 shift_x: float = 0,
+                 shift_y: float = 0) -> tuple[float, float]:
     """
-    Shifts point of xy-plane along local reference frame of the vehicle for tils compensation
-    
+    Shifts point of xy-plane along local reference frame
+    of the vehicle for tils compensation
+
     Parameters
     ----------
     x, y: float
         Coordinates of the point in global reference frame
-        
+
     alpha_x, alpha_y: float
         Angles of rotation to switch from global to local reference frames
-        
+
     shift_x, shift_y:
         Shifts along axis of local reference frame for tilt compensation
         Default: 0
-        
+
     Retruns
     ----------
     x_adj, y_adj: float
         Coordinates of the point adjusted for tilt
     """
-    
+
     # Shift along x-axis of local reference frame
     x = x + np.cos(alpha_x) * shift_x
     y = y + np.sin(alpha_x) * shift_x
-    
+
     # Shft along y-axis of local reference frame
     x_adj = x + np.cos(alpha_y) * shift_y
     y_adj = y + np.sin(alpha_y) * shift_y
-    
+
     return x_adj, y_adj
 
 
-def add_zero_element(df: pd.DataFrame, dataset: DataSet, i: int) -> None:
+def add_zero_element(df: pd.DataFrame,
+                     dataset: DataSet,
+                     i: int) -> None:
     """
     Adds zero element to the dataset wihtout corections
-    
+
     Parameters
     ----------
     df: pd.DataFrame
         Initial pandas DataFrame
-        
+
     dataset: DataSet
         Instance of class DataSet where the data will be stored
-    
+
     i: int
         The step of iteration
     """
-    curr_row = df.iloc[i]
-    shift_x, shift_y = caluclate_shifts(curr_row['roll_deg'], curr_row['pitch_deg'])
-    dataset.add_data(*curr_row[:5], shift=(shift_x, shift_y))
 
-def add_first_element(df: pd.DataFrame, dataset: DataSet, i: int) -> None:
+    curr_row = df.iloc[i]
+    shift_x, shift_y = caluclate_shifts(curr_row['roll_deg'],
+                                        curr_row['pitch_deg'])
+    dataset.add_data(*curr_row[:5],
+                     shift=(shift_x, shift_y))
+
+
+def add_first_element(df: pd.DataFrame,
+                      dataset: DataSet,
+                      i: int) -> None:
     """
     Adds first elements to the dataset
     Makes correction for zero and first elements
-    
+
     Parameters
     ----------
     df: pd.DataFrame
         Initial pandas DataFrame
-        
+
     dataset: DataSet
         Instance of class DataSet where the data will be stored
-    
+
     i: int
         The step of iteration
     """
+
     prev_row = df.iloc[i-1]
     curr_row = df.iloc[i]
     # Calculate the angle between two points
-    alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm, prev_row.y_mm,
-                                            curr_row.x_mm, curr_row.y_mm)
-    
+    alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm,
+                                      prev_row.y_mm,
+                                      curr_row.x_mm,
+                                      curr_row.y_mm)
     # Calculate the shift for current row
-    shift_x, shift_y = caluclate_shifts(curr_row.roll_deg, curr_row.pitch_deg)
+    shift_x, shift_y = caluclate_shifts(curr_row.roll_deg,
+                                        curr_row.pitch_deg)
     # Apply shift to current row
-    x_adj, y_adj = apply_shifts(curr_row.x_mm, curr_row.y_mm, alpha_x, alpha_y,
-                    shift_x, shift_y)
+    x_adj, y_adj = apply_shifts(curr_row.x_mm,
+                                curr_row.y_mm,
+                                alpha_x, alpha_y,
+                                shift_x, shift_y)
     # Record the results for current row
-    dataset.add_data(*curr_row[:5], angle=(alpha_x, alpha_y),
-                        shift=(shift_x, shift_y), adj_x=x_adj, adj_y=y_adj)
-    
+    dataset.add_data(*curr_row[:5],
+                     angle=(alpha_x, alpha_y),
+                     shift=(shift_x, shift_y),
+                     adj_x=x_adj,
+                     adj_y=y_adj)
     # Apply shift to previous row
-    x_adj_prev, y_adj_prev = apply_shifts(prev_row.x_mm, prev_row.y_mm, alpha_x, alpha_y,
-                    dataset.shift[i-1][0], dataset.shift[i-1][1])
+    x_adj_prev, y_adj_prev = apply_shifts(prev_row.x_mm, prev_row.y_mm,
+                                          alpha_x, alpha_y,
+                                          dataset.shift[i-1][0],
+                                          dataset.shift[i-1][1])
     # Recond the results for the previous row
     dataset.angle[i-1] = (alpha_x, alpha_y)
     dataset.adj_x[i-1] = x_adj_prev
     dataset.adj_y[i-1] = y_adj_prev
-    
-def recalc_prev_elements(df: pd.DataFrame, dataset: DataSet,
-                         correct_alpha_x: float, correct_alpha_y: float, i: int) -> None:
+
+
+def recalc_prev_elements(df: pd.DataFrame,
+                         dataset: DataSet,
+                         correct_alpha_x: float,
+                         correct_alpha_y: float,
+                         i: int) -> None:
     """
     Recalualtes position for the previous points
-    
+
     Parameters
     ----------
     df: pd.DataFrame
         Initial pandas DataFrame
-        
+
     dataset: DataSet
         Instance of class DataSet where the data will be stored
-        
+
     correct_alpha_x: float
-        Correct angle of rotation for x-axis between two points with the same roll
-        
+        Correct angle of rotation for x-axis between two points with same roll
+
     correct_alpha_y: float
-        Correct angle of rotation for y-axis between two points with the same roll
-    
+        Correct angle of rotation for y-axis between two points with same roll
+
     i: int
         The step of iteration
     """
-    
-    alpha_x, alpha_y = correct_alpha_x, correct_alpha_y
-    
-    for j in reversed(range(1, i)):
 
+    alpha_x, alpha_y = correct_alpha_x, correct_alpha_y
+
+    for j in reversed(range(1, i)):
         prev_row = df.iloc[j-1]
         curr_row = df.iloc[j]
-        
         # Get the shifts
         shift_x, shift_y = dataset.shift[j][0], dataset.shift[j][1]
-        
         # Apply shifts with new roll
-        x_adj, y_adj = apply_shifts(curr_row.x_mm, curr_row.y_mm, alpha_x, alpha_y,
-                shift_x, shift_y)
-        
+        x_adj, y_adj = apply_shifts(curr_row.x_mm,
+                                    curr_row.y_mm,
+                                    alpha_x,
+                                    alpha_y,
+                                    shift_x,
+                                    shift_y)
         # Update the position
         dataset.adj_x[j], dataset.adj_y[j] = x_adj, y_adj
-        
         # Update the angles for the current point in the dataframe
         # Those are the angles between current and next points
-        
         dataset.angle[j] = (alpha_x, alpha_y)
-        
         # Make a rollback (take roll from previous point)
         y_shift = calculate_backroll(dataset.roll_deg[j-1])
-        
-        x_curr_adj, y_curr_adj = apply_shifts(dataset.adj_x[j], dataset.adj_y[j],
-                                            alpha_x=dataset.angle[j][0],
-                                            alpha_y=dataset.angle[j][1],
-                                            shift_y=y_shift)
-        
+        x_curr_adj, y_curr_adj = apply_shifts(dataset.adj_x[j],
+                                              dataset.adj_y[j],
+                                              alpha_x=dataset.angle[j][0],
+                                              alpha_y=dataset.angle[j][1],
+                                              shift_y=y_shift)
         # Calculate the angle between current and previous point
-        alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm, prev_row.y_mm,
-                                        x_curr_adj, y_curr_adj)
-        
-        
+        alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm,
+                                          prev_row.y_mm,
+                                          x_curr_adj,
+                                          y_curr_adj)
     # Update separately for t=0
     # Update the angle
-    dataset.angle[0] = (alpha_x, alpha_y) # type: ignore
+    dataset.angle[0] = (alpha_x, alpha_y)  # type: ignore
     # Shift the points along that angle
-    x_adj, y_adj = apply_shifts(dataset.x_mm[0], dataset.y_mm[0], dataset.angle[0][0], dataset.angle[0][1],
-                dataset.shift[0][0], dataset.shift[0][1])
+    x_adj, y_adj = apply_shifts(dataset.x_mm[0],
+                                dataset.y_mm[0],
+                                dataset.angle[0][0],
+                                dataset.angle[0][1],
+                                dataset.shift[0][0],
+                                dataset.shift[0][1])
     # Record adjusted points
     dataset.adj_x[0], dataset.adj_y[0] = x_adj, y_adj
-    
 
-def correct_point(df: pd.DataFrame, dataset: DataSet, i: int, flag: bool, recalc=True) -> bool:
+
+def correct_point(df: pd.DataFrame,
+                  dataset: DataSet,
+                  i: int,
+                  flag: bool,
+                  recalc=True) -> bool:
     """
-    Calculates the angle between previous point (adjusted to the same roll as current) and current point
+    Calculates the angle between previous point (adjusted to the same roll
+    as current) and current point
+
     Corrects coordintes of the current point
-    If two points with same roll_deg are encountered, calculates the correct position of local reference frame
-    
+
+    If two points with same roll_deg are encountered,
+    calculates the correct position of local reference frame
+
     Parameters
     ----------
     df: pd.DataFrame
         Initial pandas DataFrame
-        
+
     dataset: DataSet
         Instance of class DataSet where the data will be stored
-    
+
     i: int
         The step of iteration
-        
+
     flag: bool
-        flag = True if the points with the same roll have not yet been encountered, False otherwise
-    
+        flag = True if the points with the same roll have not yet
+        been encountered, False otherwise
+
     recalc: bool, default = True
-        recalculate previous points' coordinates after encountering points with the same roll
-        
+        recalculate previous points' coordinates after
+        encountering points with the same roll
+
     Returns
     ----------
     flag: bool
-        flag = True if the points with the same roll have not yet been encountered, False otherwise
+        flag = True if the points with the same roll
+        have not yet been encountered, False otherwise
     """
-    
+
     prev_row = df.iloc[i-1]
     curr_row = df.iloc[i]
-    
-    
+
     if (curr_row.roll_deg == prev_row.roll_deg) & (flag):
-        
         # Calculate correct angle
-        alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm, prev_row.y_mm,
-                                            curr_row.x_mm, curr_row.y_mm)
-        
+        alpha_x, alpha_y = calc_rot_angle(prev_row.x_mm,
+                                          prev_row.y_mm,
+                                          curr_row.x_mm,
+                                          curr_row.y_mm)
+
         # Update the points for current row
-        shift_x, shift_y = caluclate_shifts(curr_row.roll_deg, curr_row.pitch_deg)
-        x_adj, y_adj = apply_shifts(curr_row.x_mm, curr_row.y_mm, alpha_x, alpha_y,
-                    shift_x, shift_y)
-        dataset.add_data(*curr_row[:5], angle=(alpha_x, alpha_y),
-                        shift=(shift_x, shift_y), adj_x=x_adj, adj_y=y_adj)
-            
+        shift_x, shift_y = caluclate_shifts(curr_row.roll_deg,
+                                            curr_row.pitch_deg)
+        x_adj, y_adj = apply_shifts(curr_row.x_mm,
+                                    curr_row.y_mm,
+                                    alpha_x,
+                                    alpha_y,
+                                    shift_x,
+                                    shift_y)
+        dataset.add_data(*curr_row[:5],
+                         angle=(alpha_x, alpha_y),
+                         shift=(shift_x, shift_y),
+                         adj_x=x_adj,
+                         adj_y=y_adj)
+
         if recalc:
             recalc_prev_elements(df, dataset, alpha_x, alpha_y, i)
-            
         flag = False
-    
+
     else:
-        
         # Get unrolled previous point
         y_shift = calculate_backroll(curr_row.roll_deg)
-        x_prev_adj, y_prev_adj = apply_shifts(dataset.adj_x[i-1], dataset.adj_y[i-1],
-                                                alpha_x=dataset.angle[i-1][0],
-                                                alpha_y=dataset.angle[i-1][1],
-                                                shift_y=y_shift)
+        x_prev_adj, y_prev_adj = apply_shifts(dataset.adj_x[i-1],
+                                              dataset.adj_y[i-1],
+                                              alpha_x=dataset.angle[i-1][0],
+                                              alpha_y=dataset.angle[i-1][1],
+                                              shift_y=y_shift)
         # Calculate angle
-        alpha_x, alpha_y = calc_rot_angle(x_prev_adj, y_prev_adj,
-                                            curr_row.x_mm, curr_row.y_mm)
-        
+        alpha_x, alpha_y = calc_rot_angle(x_prev_adj,
+                                          y_prev_adj,
+                                          curr_row.x_mm,
+                                          curr_row.y_mm)
         # Calculate the shift
-        shift_x, shift_y = caluclate_shifts(curr_row.roll_deg, curr_row.pitch_deg)
-        
+        shift_x, shift_y = caluclate_shifts(curr_row.roll_deg,
+                                            curr_row.pitch_deg)
         # Make the shift
-        x_adj, y_adj = apply_shifts(curr_row.x_mm, curr_row.y_mm, alpha_x, alpha_y,
-                    shift_x, shift_y)
-        
+        x_adj, y_adj = apply_shifts(curr_row.x_mm,
+                                    curr_row.y_mm,
+                                    alpha_x,
+                                    alpha_y,
+                                    shift_x,
+                                    shift_y)
         # Record the changes
-        dataset.add_data(*curr_row[:5], angle=(alpha_x, alpha_y),
-                        shift=(shift_x, shift_y), adj_x=x_adj, adj_y=y_adj)
-                
-    return flag     
+        dataset.add_data(*curr_row[:5],
+                         angle=(alpha_x, alpha_y),
+                         shift=(shift_x, shift_y),
+                         adj_x=x_adj,
+                         adj_y=y_adj)
 
-def transfrom(df, dataset, recalc = True):
+    return flag
+
+
+def transfrom(df: pd.DataFrame,
+              dataset: DataSet,
+              recalc: bool = True) -> None:
     """
     Iterates over the whole dataset imitating real time retrieval of data
     Updates points coordinates and calculates angles on the fly
-    
+
     Parameters
     ----------
-    
+
     df: pd.DataFrame
         Initial pandas DataFrame
-        
+
     dataset: DataSet
         Instance of class DataSet where the data will be stored
     """
-    
+
     flag = True
-    
+
     for i in range(len(df)):
-        
         if i == 0:
             add_zero_element(df, dataset, i)
-        
         if i == 1:
             add_first_element(df, dataset, i)
-            
         if i not in [0, 1]:
             flag = correct_point(df, dataset, i, flag, recalc=recalc)
-            
-            
-            
-            
-    
-    
-
-
-
-
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
 def plot_0() -> None:
     """
     Creates initial plot illustrating different reference frames
     """
+
     fig, ax = plt.subplots(figsize=(5, 5))
 
     ax.arrow(1.6, 1.4, -0.2, 0.2, width=0.01, color='black')
@@ -21,30 +23,40 @@ def plot_0() -> None:
 
     ax.set_title("Global and local reference frames")
     ax.set_xlabel('x')
-    ax.set_xlabel('y');
+    ax.set_xlabel('y')
+
 
 def EDA(df: pd.DataFrame) -> None:
     """
     Creates exploratory data analysis visualisation
-    
+
     Parameters
     ----------
     df: pd.Dataframe
         Initial DataFrame
     """
-    
+
     fig, ax = plt.subplots(2, 2, figsize=(12, 10))
 
     # Position
-    ax[0][0].scatter(df.x_mm, df.y_mm, s=20, c='orangered')
-    ax[0][0].plot(df.iloc[[0, -1]]['x_mm'], df.iloc[[0, -1]]['y_mm'], linewidth=1)
+    ax[0][0].scatter(df.x_mm,
+                     df.y_mm,
+                     s=20,
+                     c='orangered')
+    ax[0][0].plot(df.iloc[[0, -1]]['x_mm'],
+                  df.iloc[[0, -1]]['y_mm'],
+                  linewidth=1)
     ax[0][0].set_title('Position')
     ax[0][0].set_xlabel('x')
     ax[0][0].set_ylabel('y')
 
     # Update frequency
     ax[0][1].plot(df.timedelta)
-    ax[0][1].hlines(df.timedelta.mean(), xmin=0, xmax=df.shape[0], linestyles='dashed', colors=['orangered'])
+    ax[0][1].hlines(df.timedelta.mean(),
+                    xmin=0,
+                    xmax=df.shape[0],
+                    linestyles='dashed',
+                    colors=['orangered'])
     ax[0][1].set_title('Update frequency')
     ax[0][1].set_ylabel('Seconds')
     ax[0][1].set_xlabel('Timestamp')
@@ -54,22 +66,30 @@ def EDA(df: pd.DataFrame) -> None:
     ax[1][0].set_title('Roll')
     ax[1][0].set_ylabel('Degrees')
     ax[1][0].set_xlabel('Timestamp')
-    ax[1][0].hlines(df.roll_deg.mean(), xmin=0, xmax=df.shape[0], linestyles='dashed', colors=['orangered'])
+    ax[1][0].hlines(df.roll_deg.mean(),
+                    xmin=0,
+                    xmax=df.shape[0],
+                    linestyles='dashed',
+                    colors=['orangered'])
 
     # Pitch
     ax[1][1].plot(df.pitch_deg)
     ax[1][1].set_title('Pitch')
     ax[1][1].set_ylabel('Degrees')
     ax[1][1].set_xlabel('Timestamp')
-    ax[1][1].hlines(df.pitch_deg.mean(), xmin=0, xmax=df.shape[0], linestyles='dashed', colors=['orangered'])
+    ax[1][1].hlines(df.pitch_deg.mean(),
+                    xmin=0,
+                    xmax=df.shape[0],
+                    linestyles='dashed',
+                    colors=['orangered'])
 
-    fig.tight_layout();
-    
-    
+    fig.tight_layout()
+
+
 def post_plot_1(df_new: pd.DataFrame) -> None:
     """
     Creates visualization for the first task
-    
+
     Prameters
     ----------
     df_new: pd.DataFrame
@@ -78,105 +98,117 @@ def post_plot_1(df_new: pd.DataFrame) -> None:
 
     fig, ax = plt.subplots(3, 2, figsize=(12, 12))
 
-    ax = ax.ravel() #type: ignore
+    ax = ax.ravel()  # type: ignore
 
-
-    # ax 1
-    ax[0].scatter(df_new.x_mm, df_new.y_mm, s=10)
-    ax[0].scatter(df_new.adj_x, df_new.adj_y, c='r', s=10)
+    # ax 0
+    ax[0].scatter(df_new.x_mm,
+                  df_new.y_mm,
+                  s=10)
+    ax[0].scatter(df_new.adj_x,
+                  df_new.adj_y,
+                  c='r',
+                  s=10)
 
     for i in range(len(df_new.time_s)):
-        #plt.plot([dataset.x_mm[i], dataset.adj_x[i]], [dataset.y_mm[i], dataset.adj_y[i]], c='black')
-        ax[0].arrow(df_new.x_mm[i], df_new.y_mm[i], (df_new.adj_x[i]-df_new.x_mm[i]), (df_new.adj_y[i]-df_new.y_mm[i]),
-                width = 10, head_width=30)
-        
+        ax[0].arrow(df_new.x_mm[i],
+                    df_new.y_mm[i],
+                    (df_new.adj_x[i]-df_new.x_mm[i]),
+                    (df_new.adj_y[i]-df_new.y_mm[i]),
+                    width=10,
+                    head_width=30)
+
     for i in range(len(df_new)):
-        ax[0].text(df_new.iloc[i]['x_mm'] + 30, df_new.iloc[i]['y_mm'], f"pitch: {df_new.iloc[i]['pitch_deg']}, roll: {df_new.iloc[i]['roll_deg']}", fontsize=7)
+        ax[0].text(df_new.iloc[i]['x_mm'] + 30,
+                   df_new.iloc[i]['y_mm'],
+                   f"pitch: {df_new.iloc[i]['pitch_deg']}, \
+                       roll: {df_new.iloc[i]['roll_deg']}", fontsize=7)
 
     ax[0].set_xlim([6250, 10200])
     ax[0].set_ylim([-35700, -31750])
-    
     ax[0].set_title('Correction')
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('y')
-    
+
     # ax 1
-    ax[1].scatter(df_new.x_mm, df_new.y_mm, s=30)
-    ax[1].scatter(df_new.adj_x, df_new.adj_y, c='r', s=30)
-    ax[1].plot(df_new.iloc[[0, -1]].x_mm, df_new.iloc[[0, -1]].y_mm, c='orangered', linewidth=1)
-    ax[1].plot(df_new.iloc[[0, -1]].adj_x, df_new.iloc[[0, -1]].adj_y, c='royalblue', linewidth=1)
+    ax[1].scatter(df_new.x_mm,
+                  df_new.y_mm,
+                  s=30)
+    ax[1].scatter(df_new.adj_x,
+                  df_new.adj_y,
+                  c='r',
+                  s=30)
+    ax[1].plot(df_new.iloc[[0, -1]].x_mm,
+               df_new.iloc[[0, -1]].y_mm,
+               c='orangered',
+               linewidth=1)
+    ax[1].plot(df_new.iloc[[0, -1]].adj_x,
+               df_new.iloc[[0, -1]].adj_y,
+               c='royalblue',
+               linewidth=1)
 
     for i in range(len(df_new.time_s)):
-        #plt.plot([dataset.x_mm[i], dataset.adj_x[i]], [dataset.y_mm[i], dataset.adj_y[i]], c='black')
-        ax[1].arrow(df_new.x_mm[i], df_new.y_mm[i], (df_new.adj_x[i]-df_new.x_mm[i]), (df_new.adj_y[i]-df_new.y_mm[i]),
-                width = 5, head_width=10)
+        ax[1].arrow(df_new.x_mm[i],
+                    df_new.y_mm[i],
+                    (df_new.adj_x[i]-df_new.x_mm[i]),
+                    (df_new.adj_y[i]-df_new.y_mm[i]),
+                    width=5,
+                    head_width=10)
 
     ax[1].set_xlim([8500, 9500])
     ax[1].set_ylim([-35000, -34000])
-    
     ax[1].set_title('Correction zoomed')
     ax[1].set_xlabel('x')
     ax[1].set_ylabel('y')
-    
+
     # ax 2 - 5
-    
     x_shift = []
     y_shift = []
-    
     for i in range(len(df_new)):
         x_shift.append(df_new['shift'][i][0])
         y_shift.append(df_new['shift'][i][1])
-        
-    
+
     ax[2].plot(x_shift)
-    
     ax[2].set_title('Shift along x-axis')
     ax[2].set_xlabel('Timestamp')
     ax[2].set_xlabel('mm')
-    
+
     ax[3].plot(df_new['pitch_deg'])
-    
     ax[3].set_title('Pitch')
     ax[3].set_xlabel('Timestamp')
     ax[3].set_xlabel('Degrees')
-    
-    
+
     ax[4].plot(y_shift)
-    
     ax[4].set_title('Shift along y-axis')
     ax[4].set_xlabel('Timestamp')
     ax[4].set_xlabel('mm')
-    
+
     ax[5].plot(df_new['roll_deg'])
-    
     ax[5].set_title('Roll')
     ax[5].set_xlabel('Timestamp')
     ax[5].set_xlabel('Degrees')
-    
+
     fig.tight_layout()
-    
+
 
 def post_plot_2(df_new: pd.DataFrame, angles: list[float]) -> None:
     """
     Creates visualization for the second task
-    
+
     Patameters
     ----------
     df_new: pd.DataFrame
         Dataframe with recalculated values
-    
+
     angles: list[float]
         Angles between x and x'
     """
-    
+
     x_mm = df_new.adj_x
     y_mm = df_new.adj_y
-
     # calculate headings
     dx = np.diff(x_mm)
     dy = np.diff(y_mm)
     headings = np.arctan2(dy, dx)
-
     # plot points and headings as lines with arrows
     fig, ax = plt.subplots(2, 1, figsize=(8, 12))
     ax[0].scatter(x_mm, y_mm, c='orangered', s=30)
@@ -185,18 +217,26 @@ def post_plot_2(df_new: pd.DataFrame, angles: list[float]) -> None:
         y_start = y_mm[i]
         x_end = x_start + 50*np.cos(headings[i])
         y_end = y_start + 50*np.sin(headings[i])
-        ax[0].arrow(x_start, y_start, x_end-x_start, y_end-y_start, head_width=50, head_length=50, width=10, fc='royalblue', ec='royalblue')
+        ax[0].arrow(x_start,
+                    y_start,
+                    x_end-x_start,
+                    y_end-y_start,
+                    head_width=50,
+                    head_length=50,
+                    width=10,
+                    fc='royalblue',
+                    ec='royalblue')
 
     ax[0].set_xlim(6250, 10200)
     ax[0].set_ylim(-35700, -31750)
-    
+
     ax[0].set_title('Vehicle heading')
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('y')
-    
+
     ax[1].plot(angles)
     ax[1].set_title("Vehicle heading (angle between $x$ and $x'$)")
     ax[1].set_xlabel('Timestamp')
     ax[1].set_ylabel('Angle(degrees)')
-    
+
     fig.tight_layout()
